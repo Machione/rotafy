b080ee0073ca4bf3e5164cb394c0bedf1f8dde0d
Ryan McKeown <ryanmckeown@mail4me.com>
2024-07-22 21:46:28 +0100

    Test human_readable_date method

--- tests/rota/test_printable.py
+++ tests/rota/test_printable.py
@@ -1,4 +1,5 @@
 import pytest
+import datetime
 from rotafy.rota import printable, rota
 
 
@@ -33,3 +34,20 @@ def test_ordinal(test_printable):
     assert test_printable._ordinal(23) == "23rd"
     for x in range(24, 31):
         assert test_printable._ordinal(x) == str(x) + "th"
+
+
+def test_human_readable_date(test_printable):
+    today = datetime.date.today()
+    ordinal = test_printable._ordinal(today.day)
+    day = today.strftime("%A")
+    month = today.strftime("%B")
+    year = today.strftime("%Y")
+
+    f_t = test_printable._human_readable_date(today)
+    assert f_t == f"{ordinal} {month} {year}"
+    t_t = test_printable._human_readable_date(today, True)
+    assert t_t == f"{day} {ordinal} {month} {year}"
+    t_f = test_printable._human_readable_date(today, True, False)
+    assert t_f == f"{day} {ordinal} {month}"
+    f_f = test_printable._human_readable_date(today, False, False)
+    assert f_f == f"{ordinal} {month}"
