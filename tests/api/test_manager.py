23617ac34bf337ca24b85ed3898b308089f46288
Ryan McKeown <ryanmckeown@mail4me.com>
2024-07-27 17:26:47 +0100

    Add placeholder tests to be completed later

---
+++ tests/api/test_manager.py
@@ -0,0 +1,62 @@
+import pytest
+from rotafy.api import manager
+
+
+def test_init():
+    raise NotImplementedError()
+
+
+def test_print():
+    raise NotImplementedError()
+
+
+def test_to_pdf():
+    raise NotImplementedError()
+
+
+def test_chores_on():
+    raise NotImplementedError()
+
+
+def test_find_assignment():
+    raise NotImplementedError()
+
+
+def test_remove_person():
+    raise NotImplementedError()
+
+
+def test_add_person():
+    raise NotImplementedError()
+
+
+def test_swap():
+    raise NotImplementedError()
+
+
+def test_replace():
+    raise NotImplementedError()
+
+
+def test_check_and_heal():
+    raise NotImplementedError()
+
+
+def test_all_independently_valid_assignments():
+    raise NotImplementedError()
+
+
+def test_row_weight():
+    raise NotImplementedError()
+
+
+def test_assign_chores_on():
+    raise NotImplementedError()
+
+
+def test_fill():
+    raise NotImplementedError()
+
+
+def test_notify():
+    raise NotImplementedError()
